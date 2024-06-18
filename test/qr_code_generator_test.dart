1e0f8f4eafcf88fdb5ecfb6c13cee1a4532e9ea2
baishakhee93 <baishu9534@gmail.com>
2024-06-18 15:02:45 +0530

    test cases modify

--- test/qr_code_generator_test.dart
+++ test/qr_code_generator_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qr_generate_sample/main.dart';
 
+
 void main() {
   testWidgets('QR Code Generator shows QR code', (WidgetTester tester) async {
     // Build the widget tree
